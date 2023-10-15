f175e6fa19c94c6ebb3d74840cbbde08d9f8d7db
al-ell <>
2023-10-15 11:47:07 +0000

    feat: question base added

---
+++ assets/javascript/main.js
@@ -0,0 +1,93 @@
+
+const easyQuizArray = [
+    {
+        id: "1",
+        question: "What is the capital of Russia?",
+        a: "Moscow",
+        b: "Madrid",
+        c: "Warsaw",
+        d: "Berlin",
+        correct: "a",
+    },
+    {
+        id: "2",
+        question: "Which of these rivers is the longest?",
+        a: "Amazon", 
+        b: "Congo", 
+        c: "Yangtze", 
+        d: "Yellow",
+        answer: "a"
+    },
+    {
+        id: "3",
+        question: "Athens is the capital of which country?",
+        a: "Argentina", 
+        b: "Egypt", 
+        c: "Greece",
+        d: "Kenya",
+        answer: "c"
+    },
+    {
+        id: "4",
+        question: "What is the name of the country that borders the United States to the north?",
+        a: "Columbia", 
+        b: "Russia", 
+        c: "Canada", 
+        d: "Mexico",
+        answer: "c"
+    },
+    {
+        id: "5",
+        question: "What three colors do you find on the Italian flag?",
+        a: "Red, White, Green",
+        b: "Red, White, Yellow",
+        c: "Red, White, Blue",
+        d: "Red, White, Black",
+        answer: "a"
+    },
+    {
+        id: "6",
+        question: "Where is the driest place on earth?",
+        a: "Red, White, Green",
+        b: "Red, White, Yellow",
+        c: "Red, White, Blue",
+        d: "Red, White, Black",
+        answer: "a"
+    },
+    {
+        id: "7",
+        question: "What is the largest continent",
+        a: "North America",
+        b: "Asia",
+        c: "Europe",
+        d: "Africa",
+        answer: "b"
+    },
+    {
+        id: "8",
+        question: "The famous 'Leaning Tower of Pisa' is found in what country?",
+        a: "Spain",
+        b: "Germany ",
+        c: "Italy",
+        d: "France",
+        answer: "c"
+    },
+    {
+        id: "9",
+        question: "Which of the Earth's oceans is the smallest in size?",
+        a: "Southern",
+        b: "Indian",
+        c: "Pacific",
+        d: "Arctic",
+        answer: "d"
+    },
+    {
+        id: "10",
+        question: "The nation state of Slovenia is located on which continent?",
+        a: "Europe",
+        b: "Asia",
+        c: "Africa",
+        d: "South America",
+        answer: "c"
+    },
+];
